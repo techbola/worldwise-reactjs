8d47c98d677af848fe2e274be555ef6c07d9bb07
Adebola <bola@neegles.com>
2024-10-11 05:16:36 +0100

    app layout design

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+import styles from "./Sidebar.module.css";
+
+export default function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <p>List of cities</p>
+
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          Copyright &copy; {new Date().getFullYear()}. All rights reserved.
+        </p>
+      </footer>
+    </div>
+  );
+}
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,3 +1,12 @@
+import Sidebar from "../components/Sidebar";
+import Map from "../components/Map";
+import styles from "./AppLayout.module.css";
+
 export default function AppLayout() {
-  return <div>AppLayout</div>;
+  return (
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
+    </div>
+  );
 }

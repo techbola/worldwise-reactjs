209ebe02d44094d61765d65a3de2bf57212455de
Adebola <bola@neegles.com>
2024-10-15 22:15:16 +0100

    implement authentication

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -135,7 +135,7 @@ function CitiesProvider({ children }) {
 function useCities() {
   const context = useContext(CitiesContext);
   if (context === undefined) {
-    throw new Error("useCities must be used within a CitiesProvider");
+    throw new Error("CitiesContext must be used within a CitiesProvider");
   }
   return context;
 }
